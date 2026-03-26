b605d1d6dd9937b8373c0e03cd089beed5ea0a16
Bartłomiej Dobrowolski <135438002+dixe1@users.noreply.github.com>
2026-03-26 21:06:04 +0100

    feat: change getValue() with get<T>()

--- ConfigLoader.h
+++ ConfigLoader.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <unordered_map>
+#include <array>
 #include <any>
 
 class ConfigLoader
@@ -9,15 +10,22 @@ public:
     static std::unordered_map<std::string, std::any>
     load(const std::string& filePath);
 
-    std::any getValue(const std::string& key);
-
     ConfigLoader(const std::string& filePath)
     {
         config = load(filePath);
     }
 
+    std::any getValue(const std::string& key);
+
+    template<typename T>
+    T get(const std::string &key)
+    {
+        std::any value = getValue(key);
+        return std::any_cast<T>(value);
+    }
+
 private:
-    std::unordered_map<std::string, std::any> config;
+    std::unordered_map<std::string, std::any> config;   // Main config variable
 
     static void checkFile(const std::ifstream& file, const std::string& filePath);
     static void deleteSpaces(std::string& line);
